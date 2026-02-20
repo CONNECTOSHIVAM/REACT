ba8f25c843e7e822c6dc0ea0f1845ecb8bcbe38a
connectoshivam <connecttoshivam1@gmail.com>
2026-02-20 10:54:28 +0530

    basics

---
+++ ProCoder/03Tailwind_Props/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1 className='text-orange-500 text-5xl bg-yellow-50 p-5 rounded-2xl'>radhe radhe</h1>
+    </>
+  )
+}
+
+export default App
