45cfcbeb47cf9b3c2c67702ae8ae988a3b3184fa
connectoshivam <connecttoshivam1@gmail.com>
2026-02-20 11:41:51 +0530

    bgChanger

--- ProCoder/04BgChanger/src/App.jsx
+++ ProCoder/04BgChanger/src/App.jsx
@@ -7,10 +7,23 @@ function App() {
   const [color, setColor] = useState('blue')
 
   return (
-    <>
-      <div className=''iv>
-    </>
+
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className="flex flex-wrap justify-center gap-3 shadow-2xl bg-amber-50 px-2 py-3 rounded-2xl">
+          <button className='outline-none px-5 py-1 rounded-2xl text-white shadow-2xl' style={{ backgroundColor: "red" }} onClick={() => setColor("red")}> red</button>
+          <button className='outline-none px-5 py-1 rounded-2xl text-white shadow-2xl' style={{ backgroundColor: "wheat" }} onClick={() => setColor("wheat")}> wheat</button>
+          <button className='outline-none px-5 py-1 rounded-2xl text-white shadow-2xl' style={{ backgroundColor: "goldenrod" }} onClick={() => setColor("goldenrod")}> goldenrod</button>
+          <button className='outline-none px-5 py-1 rounded-2xl text-white shadow-2xl' style={{ backgroundColor: "black" }} onClick={() => setColor("black")}> red</button>
+          <button className='outline-none px-5 py-1 rounded-2xl text-white shadow-2xl' style={{ backgroundColor: "olive" }} onClick={() => setColor("olive")}> red</button>
+          <button className='outline-none px-5 py-1 rounded-2xl text-white shadow-2xl' style={{ backgroundColor: "teal" }} onClick={() => setColor("teal")}> red</button>
+          <button className='outline-none px-5 py-1 rounded-2xl text-white shadow-2xl' style={{ backgroundColor: "orange" }} onClick={() => setColor("orange")}> red</button>
+
+        </div>
+      </div>
+    </div>
+
   )
 }
 
-export default App
+export default App;
