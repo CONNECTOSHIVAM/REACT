827f9b080fb75fa1be185d8b502d30519b2e1412
connectoshivam <connecttoshivam1@gmail.com>
2026-03-03 23:50:32 +0530

    radhe radhe

--- ProCoder/01radharadha/src/App.jsx
+++ ProCoder/01radharadha/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <>
-      <h1>radhe radhe</h1>
+      <div className='w-full max-w-md mx-auto my-4 shadow-lg rounded-2xl p-7 my-6 bg-orange-500 text-amber-100 hover:bg-orange-600'>
+         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Radha_Krishna_at_Iskcon_Vrindavan.jpg/960px-Radha_Krishna_at_Iskcon_Vrindavan.jpg" alt="radhe radhe" />
+         <h1 className='text-3xl p-5 '>radhe radhe</h1>
+      </div>
     </>
   )
 }
