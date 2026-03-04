e31ae8a237363990550afbd64408593ab62e1a22
connectoshivam <connecttoshivam1@gmail.com>
2026-03-04 20:28:14 +0530

    update the files

--- ProCoder/08CardApp/src/App.jsx
+++ ProCoder/08CardApp/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserCard from './components/UserCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className='grid grid-cols-4 gap-[1rem] justify-around'>
+        <UserCard/>
+        <UserCard/>
+        <UserCard/>
+        <UserCard/>
+        <UserCard/>   
+      </div>
     </>
   )
 }
---
+++ ProCoder/08CardApp/src/components/UserCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='w-[20rem] h-[30rem] bg-amber-100 rounded-2xl justify-around m-10'>
+       
+    </div>
+  )
+}
+
+export default UserCard
