245a0dfc240e2a69ebd884239e94552613693d8f
connectoshivam <connecttoshivam1@gmail.com>
2026-03-06 09:03:58 +0530

    update the files.

--- ProCoder/09colorChanger/src/App.jsx
+++ ProCoder/09colorChanger/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Buttons from './components/Buttons'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+       <>
        <h1>Color Crunch 🎨</h1>
+       <Buttons label="orange"/>
+       <Buttons label="Gold"/>
+       <Buttons label="sky"/>
+       </>
   )
 }
 
--- ProCoder/09colorChanger/src/components/Buttons.jsx
+++ ProCoder/09colorChanger/src/components/Buttons.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Buttons = () => {
+const Buttons = (props) => {
   return (
     <div>
-      
+      <button className="bg-amber-200 p-5 rounded-3xl">{props.label}</button>
     </div>
   )
 }
