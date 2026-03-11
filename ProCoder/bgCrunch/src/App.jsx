5f9d001b5edcd08b9b61b18af2eafc5cf1da0df9
connectoshivam <connecttoshivam1@gmail.com>
2026-03-11 18:14:42 +0530

    add the Quiz app vite app files

---
+++ ProCoder/bgCrunch/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-amber-500 text-5xl text-center p-5 font-mono w-full m-auto'>Quiz App</h1>
+    </div>
+  )
+}
+
+export default App
