5c1e68721604fbc949df166a2024f4cd29d3756b
connectoshivam <connecttoshivam1@gmail.com>
2026-03-11 20:42:00 +0530

    add the timer files

--- ProCoder/bgCrunch/src/App.jsx
+++ ProCoder/bgCrunch/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Timer from './components/Timer'
 
 const App = () => {
   return (
     <div>
-      <h1 className='bg-amber-500 text-5xl text-center p-5 font-mono w-full m-auto'>Quiz App</h1>
+      <h1 className='text-amber-700 underline text-orange-500 text-5xl text-center p-5 font-mono w-full m-auto'>Quiz App</h1>
+      <Timer />
     </div>
   )
 }
--- ProCoder/bgCrunch/src/components/Timer.jsx
+++ ProCoder/bgCrunch/src/components/Timer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Timer = () => {
+
+    const [leftTime, setLeftTime] = useState(15);
+    const [isRunning, setIsRunning] = useState(true);
+
+    //left time logic
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            setLeftTime((prev)=> {
+                if(prev === 0){
+                    clearInterval(timer);
+                    return 0
+                }
+                return prev - 1
+            })
+        }, 1000)
+        return () => clearInterval(timer)
+    }, [])
+ 
+    // time formatting logic
+    useEffect(()=>{
+        let formattedTime = (`${Math.floor(leftTime/60).toString().padStart(2, '0')} min  ${(leftTime%60).toString().padStart(2, '0')} sec`);
+        setIsRunning(formattedTime)
+    },[leftTime])
+
   return (
-    <div>
-      
+    <div className='flex items-center  justify-center'>
+    
+      <h1 className='text-center w-[40%] rounded-2xl bg-amber-600 py-6 m-6 text-3xl font-bold text-blue-950 border-3 hover:bg-amber-700 '>Time Left 🕦: {isRunning}</h1>
     </div>
   )
 }
