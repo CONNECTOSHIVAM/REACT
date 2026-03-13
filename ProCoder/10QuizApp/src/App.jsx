338a0a366da93c49ed1b57cb433d46444644ce64
connectoshivam <connecttoshivam1@gmail.com>
2026-03-13 14:19:45 +0530

    update the questions.jsx files

--- ProCoder/10QuizApp/src/App.jsx
+++ ProCoder/10QuizApp/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Timer from './components/Timer'
+import Questions from './components/Questions'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
      <h1 className='text-5xl font-mono text-center text-sky-500 mt-7 border-double shadow-amber-50 border-4 m-auto w-[30%] rounded-xl p-5 hover:border-sky-300 duration-300 ease-out'>Quiz App</h1>
      <Timer />
+     <Questions/>
     </>
   )
 }
---
+++ ProCoder/10QuizApp/src/components/Questions.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import question from "./questions.json";
+
+const Questions = ({ setIsOver, setScore }) => {
+  const [currrentIndex, setCurrentIndex] = useState(0);
+
+  const handelOptionClick = (selectedOption) => {
+    if (selectedOption === question[currrentIndex].answer) {
+      setScore((prev) => prev + 1);
+    }
+    if (currrentIndex < question.length - 1) {
+      setCurrentIndex((prev) => prev + 1);
+    } else {
+      setIsOver(true);
+    }
+  };
+  return (
+    <div>
+      <h2 className="text-3xl text-white font-mono text-center mt-7">
+        Q.{question[currrentIndex].question}
+      </h2>
+      <div className="flex justify-center items-center mt-15 gap-6">
+        {question[currrentIndex].options.map((option) => {
+          return (
+            <button
+              className="w-35 p-4 border-4 border-double border-sky-400 text-sky-400 rounded-2xl  hover:border-amber-50 hover:text-amber-50 duration-500 ease-out"
+              key={option}der-double
+              onClick={() => {
+                handelOptionClick(option);
+              }}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
