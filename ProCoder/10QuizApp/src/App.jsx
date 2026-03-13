a29714837927bea6b2d3eaacff859749abfd9565
connectoshivam <connecttoshivam1@gmail.com>
2026-03-13 12:27:58 +0530

    add the Quiz app files

--- ProCoder/10QuizApp/src/App.jsx
+++ ProCoder/10QuizApp/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <h1 className='text-5xl font-mono text-center text-sky-500 mt-7 border-double shadow-amber-50 border-4 m-auto w-[30%] rounded-xl p-5 hover:border-sky-300 duration-300 ease-out'>Quiz App</h1>
     </>
   )
 }
