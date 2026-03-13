5abbf9dd05717cd5ce2bdea89c68c13279e55b65
connectoshivam <connecttoshivam1@gmail.com>
2026-03-13 13:42:52 +0530

    add the timer filess

--- ProCoder/10QuizApp/src/App.jsx
+++ ProCoder/10QuizApp/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Timer from './components/Timer'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <h1 className='text-5xl font-mono text-center text-sky-500 mt-7 border-double shadow-amber-50 border-4 m-auto w-[30%] rounded-xl p-5 hover:border-sky-300 duration-300 ease-out'>Quiz App</h1>
+     <Timer />
     </>
   )
 }
---
+++ ProCoder/10QuizApp/src/components/Timer.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+const Timer = () => {
+
+    const [leftTime, setLeftTime] = useState(20);
+    const [displayTime, setDisplayTime] = useState('');
+
+    //left time logic
+    useEffect(()=>{
+        let intervalid = setInterval(()=>{
+            setLeftTime(prev => {
+                if(prev <= 0){
+                    clearInterval(intervalid)
+                    return 0;
+                }
+                return prev - 1;
+            })
+        },1000);
+
+        return () => {
+            clearInterval(intervalid)
+        }
+    },[])
+
+    // Time format logic
+    useEffect(()=>{
+
+        if(leftTime === 0){
+            setIsOver(true);
+        }
+        let formattedTime = (`${(Math.floor(leftTime/60)).toString().padStart(2,0)}: ${(leftTime%60).toString().padStart(2, 0)}`)
+        setDisplayTime(formattedTime)
+    },[leftTime])
+   return (
+    <div>
+      <h1 className='text-center text-2xl text-sky-800 m-5 font-mono font-bold'>Time Left🕦: {displayTime}</h1>
+    </div>
+  )
+}
+
+export default Timer
