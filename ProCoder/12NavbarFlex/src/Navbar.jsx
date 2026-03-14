a04f2fe0056ffd7447c7aed7271a4ea71bddec1d
connectoshivam <connecttoshivam1@gmail.com>
2026-03-14 23:33:40 +0530

    update the files code

--- ProCoder/12NavbarFlex/src/Navbar.jsx
+++ ProCoder/12NavbarFlex/src/Navbar.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-around'>
-      <h1>Logo</h1>
+    <div className='flex justify-between items-center p-[2rem] w-full'>
+      <h1 className='text-5xl font-extrabold font-mono text-amber-50 relative cursor-pointer group hover:opacity-85'>Logo <span className='absolute left-0 -bottom-1 h-[5px] w-0 bg-pink-500 rounded-full transition-all duration-500 ease-out group-hover:w-full '></span></h1>
 
-      <div>
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Services</a>
+      <div className=' flex text-3xl relative text-amber-50 text-center gap-10 cursor-pointer group'>
+        <a href="">Home <span className='absolute left-0 -bottom-1 h-[3px] w-0 bg-pink-500 rounded-full transition-all duration-500 ease-out group-hover:w-full'></span></a>
+        <a href="">About <span className='absolute left-0 -bottom-1 h-[3px] w-0 bg-pink-500 rounded-full transition-all duration-500 ease-out group-hover:w-full'></span></a>
+        <a href="">Services <span className='absolute left-0 -bottom-1 h-[3px] w-0 bg-pink-500 rounded-full transition-all duration-500 ease-out group-hover:w-full'></span></a>
         <a href="">Contact</a>
         <a href="">Login</a>
       </div>
 
-      <div>
-        <span>🔍</span>
-        <span>📄 </span>
+      <div className='flex text-3xl gap-8 '>
+        <span className='hover:opacity-80'>🔍</span>
+        <span className='hover:opacity-80'>📄 </span>
       </div>
     </div>
   )
