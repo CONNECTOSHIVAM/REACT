be3ab89633e8f2587f044321ee4262730b501588
connectoshivam <connecttoshivam1@gmail.com>
2026-03-14 08:28:48 +0530

    add perposal flex files

--- ProCoder/11RenderingFlex/src/App.jsx
+++ ProCoder/11RenderingFlex/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
+import Popup from "./Popup";
 
 function App() {
-  const [popup, setPopup] = useState(true);
+  const [popup, setPopup] = useState(false);
+  const [triggerPopup, setTriggerPopup] = useState(false);
 
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center h-screen gap-8">
         <button onClick={()=>{setPopup(true)}} className="w-55 p-4 bg-amber-400 text-2xl border-3 font-mono font-bold text-sky-900 border-[#d97706] rounded-3xl hover:border-sky-600 duration-500 ease-out">
           punch me
         </button>
+        <button onClick={()=>{setTriggerPopup(true)}} className="w-55 p-4 bg-amber-400 text-2xl border-3 font-mono font-bold text-sky-900 border-[#d97706] rounded-3xl hover:border-sky-600 duration-500 ease-out">
+          trigger me
+        </button>
       </div>
+
+      <Popup popup={popup} setPopup={setPopup} title={"radhe radhe🙏"} description={"/P Maine Google kiya — 'how to stop thinking about someone.' 1 crore results aaye, koi kaam nahi aaya. Tumhara naam search kiya — dil ne bola 'yahi answer hai bhai.' Toh seriously yaar, kya hum officially ek baar try kar sakte hain? Worst case — dono friends rahenge. Best case — best story banegi. Deal hai? 😂❤️"} />
+      <Popup popup={triggerPopup} setPopup={setTriggerPopup} title={"radhe radhe🙏"} description={"/T Suno, main toh bas timepass kar raha tha life mein — sab theek tha. Phir tum aaye, muskuraye, kuch bola kiya aur dil ne bola 'bhai ab game over.' Seriously yaar, yeh sab tumhari galti hai. Ab ek kaam karo — meri life mein officially aa jao, unofficially toh aa hi chuke ho. 😄🌸"} />
 
     </>
   );
--- ProCoder/11RenderingFlex/src/Popup.jsx
+++ ProCoder/11RenderingFlex/src/Popup.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
 
-const Popup = ({}) => {
+const Popup = ({popup, setPopup, title, description}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      {popup && (
+        <div className="absolute top-0 left-0 w-screen h-screen bg-[#121212] flex justify-center items-center">
+          <button
+            onClick={() => {
+              setPopup(false);
+            }}
+            className="rounded-2xl hover:opacity-70 border-2 border-double text-[tomato] p-4 text-center absolute top-[1rem] right-[1rem]"
+          >
+            ❌
+          </button>
 
-export default Popup
+          <div className="w-full h-full max-w-[45rem] max-h-[25rem] rounded-b-4xl bg-[#272626] shadow-2xs hover:border-2 text-blue-400 duration-300 ease-linear">
+            <h1 className="text-center text-4xl text-orange-600 font-mono font-bold mt-8">
+              {title}
+            </h1>
+            <p className="text-center text-lg text-amber-100 mt-4 font-serif ">
+              {description}
+            </p>
+
+            <div className="flex items-center justify-center gap-8 mt-10 ">
+              <button className="w-32 p-4 bg-emerald-400 text-lg font-sans font-bold text-sky-900 border-[#10b981] rounded-2xl hover:opacity-70 duration-500 ease-out">
+                Love
+              </button>
+              <button className="w-32 p-4 bg-red-400 text-lg font-sans font-bold text-sky-900 border-[#ef4444] rounded-2xl hover:opacity-70 duration-500 ease-out ">
+                Sachii..
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Popup;
