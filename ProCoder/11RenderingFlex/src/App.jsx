e274c565c107d1340064fdee9b8e2b3191ca4aeb
connectoshivam <connecttoshivam1@gmail.com>
2026-03-14 07:22:26 +0530

    update the files

--- ProCoder/11RenderingFlex/src/App.jsx
+++ ProCoder/11RenderingFlex/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [popup, setPopup] = useState(true);
 
   return (
     <>
-    <div className='flex items-center justify-center h-screen'>
-        <button className='w-55 p-4 bg-amber-400 text-2xl border-3 font-mono font-bold text-gray-900 border-[#d97706] rounded-3xl'>punch me</button>
-    </div>
+      <div className="flex items-center justify-center h-screen">
+        <button onClick={()=>{setPopup(true)}} className="w-55 p-4 bg-amber-400 text-2xl border-3 font-mono font-bold text-sky-900 border-[#d97706] rounded-3xl hover:border-sky-600 duration-500 ease-out">
+          punch me
+        </button>
+      </div>
+
+      {popup && (
+        <div className="absolute top-0 left-0 w-screen h-screen bg-[#121212] flex justify-center items-center">
+          <button onClick={()=>{setPopup(false)}} className="rounded-2xl hover:opacity-70 border-2 border-double text-[tomato] p-4 text-center absolute top-[1rem] right-[1rem]">❌</button>
+
+          <div className="w-full h-full max-w-[45rem] max-h-[25rem] rounded-b-4xl bg-[#272626] shadow-2xs hover:border-2 text-blue-400 duration-300 ease-linear">
+            <h1 className="text-center text-4xl text-orange-600 font-mono font-bold mt-8">radhe radhe🙏</h1>
+            <p className="text-center text-lg text-amber-100 mt-4 font-serif ">Hanji, kaise ho aap? &nbsp; ek baat batau.. aap se Love sove ho gaya hai ji.. </p>
+
+            <div className="flex ">
+              <button className="w-32 p-4 bg-emerald-400 text-lg font-sans font-bold text-sky-900 border-[#10b981] rounded-2xl hover:border-sky-600 duration-500 ease-out">Love</button>
+              <button className="w-32 p-4 bg-red-400 text-lg font-sans font-bold text-sky-900 border-[#ef4444] rounded-2xl hover:border-sky-600 duration-500 ease-out ">Sachii..</button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
