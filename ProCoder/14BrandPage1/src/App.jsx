f2d30a4467eef4c7066884db2cd93961961bcd2a
connectoshivam <connecttoshivam1@gmail.com>
2026-05-17 22:45:24 +0530

    setup the initial projects files

--- ProCoder/14BrandPage1/src/App.jsx
+++ ProCoder/14BrandPage1/src/App.jsx
@@ -2,23 +2,13 @@ import React from 'react'
 
 import "./App.css"
 
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
 const App = () => {
   return (
     <div>
-      <nav className="container">
-        <div className="logo">
-          <img src="/images/brand_logo.png" alt="brand logo" />
-        </div>
-
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>login</button>
-      </nav>
+      <Navigation />
+      <Hero />
     </div>
   )
 }
