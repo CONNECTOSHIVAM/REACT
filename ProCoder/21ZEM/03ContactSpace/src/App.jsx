0463f01fbfe5c5df0312f4c495d2658694cff7ab
connectoshivam <connecttoshivam1@gmail.com>
2026-05-24 23:28:29 +0530

    setup the navigation section

---
+++ ProCoder/21ZEM/03ContactSpace/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './components/Navigation/Navigation'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+    </div>
+  )
+}
+
+export default App
