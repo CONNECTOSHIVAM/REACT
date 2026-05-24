e3591ddc572d473dd6a56af4d836c8d5f925eb78
connectoshivam <connecttoshivam1@gmail.com>
2026-05-24 23:42:15 +0530

    setup the contactHead section

--- ProCoder/21ZEM/03ContactSpace/src/App.jsx
+++ ProCoder/21ZEM/03ContactSpace/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Navigation from './components/Navigation/Navigation'
 
 import './App.css'
+import Navigation from './components/Navigation/Navigation'
+import ContactHeader from './components/ContactHeader/ContactHeader'
 
 const App = () => {
   return (
     <div>
       <Navigation/>
+      <ContactHeader />
     </div>
   )
 }
---
+++ ProCoder/21ZEM/03ContactSpace/src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './ContactHeader.module.css'
+
+const ContactHeader = () => {
+  return (
+    <header className={`${styles.contact_section} container`}>
+        <h1>CONTACT US</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, corrupti aliquam natus earum impedit sint dignissimos eum nam. Amet corporis rem facilis nesciunt laudantium earum suscipit fuga aut voluptate magnam?</p>
+
+    </header>
+  )
+}
+
+export default ContactHeader
