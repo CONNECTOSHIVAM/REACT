45d1f89a5567d1b82802088f218d93804bc4157a
connectoshivam <connecttoshivam1@gmail.com>
2026-06-11 21:34:57 +0530

    initail setup of files and design the project structure

---
+++ ProCoder/27TodoSpace/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-amber-100 h-21 text-5xl text-center'>radhe radhe</h1>
+    </div>
+  )
+}
+
+export default App
