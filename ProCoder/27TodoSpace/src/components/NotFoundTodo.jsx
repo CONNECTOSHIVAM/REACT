a54d207d81dc9f3809e96d64da7da2a63d85d247
connectoshivam <connecttoshivam1@gmail.com>
2026-06-13 02:07:06 +0530

    update the NotFoundTodo.jsx file

--- ProCoder/27TodoSpace/src/components/NotFoundTodo.jsx
+++ ProCoder/27TodoSpace/src/components/NotFoundTodo.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { RiTodoFill } from "react-icons/ri";
 
 const NotFoundTodo = () => {
-  return <div></div>;
+  return( 
+  <div className="flex flex-col justify-center items-center m-auto h-[50vh]">
+      <RiTodoFill className="text-3xl text-amber-900"/>
+      <h1 className="text-2xl font-medium text-amber-600" >todo not found!</h1>
+  </div>);
 };
 
 export default NotFoundTodo;
