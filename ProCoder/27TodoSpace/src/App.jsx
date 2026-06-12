2e193f1afd9ba81bc8356189389e844d6155cb1b
connectoshivam <connecttoshivam1@gmail.com>
2026-06-12 21:51:28 +0530

    setup the Navbar.jsx file

--- ProCoder/27TodoSpace/src/App.jsx
+++ ProCoder/27TodoSpace/src/App.jsx
@@ -18,15 +18,29 @@ const App = () => {
   const {isOpen, onClose, OnOpen} = useDisclosure();
 
 
+  const status = useMemo(()=>{
+
+    const total = todos.length;
+
+    const completed = todos.filter((todo)=>{
+      todo.status === "completed"
+    }).length;
+
+    const pending = total - completed;
+
+    return {total, completed, pending};
+  },[todos])
+
+
 
   
   
 
 
   return (
-    <div>
+    <div className='max-w-[710px] mx-auto px-4 pb-10'>
       {/* <h1 className='bg-amber-100 h-21 text-5xl text-center'>radhe radhe</h1> */}
-      <Navbar/>
+      <Navbar totalTodos={status.total}/>
     </div>
   )
 }
