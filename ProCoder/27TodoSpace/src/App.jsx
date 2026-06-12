6d85408801c12e0b659fc36f96994ac008215bb5
connectoshivam <connecttoshivam1@gmail.com>
2026-06-12 11:59:46 +0530

    update the app.jsx files.

--- ProCoder/27TodoSpace/src/App.jsx
+++ ProCoder/27TodoSpace/src/App.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import useTodos from './hooks/useTodos';
+import useDisclosure from './hooks/useDisclosure';
+import { collection } from 'firebase/firestore';
+import { db } from './config/firebase';
+
 
 
 const App = () => {
 
-  const [todos, loading] = useTodos();
+  const {todos, loading} = useTodos();
 
   const [tab, setTab] = useState("todo");
   const [search, setSearch] = useState("");
+
+  const {isOpen, onClose, OnOpen} = useDisclosure();
+
+
+
+  const filteredTodos = useMemo(()=>{
+
+    const searchTerm = search.trim().toLowerCase();
+
+    return todos.filter(
+      (todo) => 
+        todo.status === tab &&   tab.title.toLowerCase().includes(searchTerm)
+    );
+  },[todos, tab, search])
+  
 
 
   return (
